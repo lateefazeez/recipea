9d0c6e092a9c3cd73932a38482d7fbc4ba97629e
Lateef Azeez <codelate18@gmail.com>
2021-12-04 18:45:02 -0700

    create navbar component

--- recipee/src/App.js
+++ recipee/src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
-import { Routes, Route, Link } from "react-router-dom"
+import { Routes, Route } from "react-router-dom"
 import Home from './pages/home/Home'
 import Create from './pages/create/Create'
 import Recipe from './pages/recipe/Recipe'
 import Search from './pages/search/Search'
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
     <Routes>
       <Route exact path="/" element={ <Home />} />
       <Route path="/create" element={ <Create />} />
---
+++ recipee/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom"
+import './Navbar.css'
+
+const Navbar = () => {
+  return ( 
+    <div className="navbar">
+      <nav>
+        <Link to="/" className="brand">
+          <h1>Recipea</h1>
+        </Link>
+        <Link to="/create">Create</Link>
+      </nav>
+    </div>
+    
+   );
+}
+ 
+export default Navbar;
