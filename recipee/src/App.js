0ceef523f6277f94219a287d5cf73a546ce67b32
Lateef Azeez <codelate18@gmail.com>
2021-12-04 18:22:20 -0700

    create the routes and add starter codes

--- recipee/src/App.js
+++ recipee/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route, Link } from "react-router-dom"
+import Home from './pages/home/Home'
+import Create from './pages/create/Create'
+import Recipe from './pages/recipe/Recipe'
+import Search from './pages/search/Search'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Routes>
+      <Route exact path="/" element={ <Home />} />
+      <Route path="/create" element={ <Create />} />
+      <Route path="/recipes/:id" element={ <Recipe />} />
+      <Route path="search" element={ <Search />} />
+    </Routes>
     </div>
   );
 }
